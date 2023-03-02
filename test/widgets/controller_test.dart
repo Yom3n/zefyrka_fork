8fa1c561b9de2a06812a2a03101b4ca4cbfa2688
Kamil <kamilek41@gmail.com>
2023-03-02 12:24:35 +0100

    fixed build

--- test/widgets/controller_test.dart
+++ test/widgets/controller_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:quill_format/quill_format.dart';
 import 'package:zefyrka/zefyrka.dart';
 
 void main() {
