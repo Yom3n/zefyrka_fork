813292135c5c85785be5d00adba012c2fc76a03a
Kamil <kamilek41@gmail.com>
2026-09-24 14:09:13 +0200

    improved tests

--- test/widgets/editor_input_client_test.dart
+++ test/widgets/editor_input_client_test.dart
@@ -1,8 +1,24 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:quill_format/quill_format.dart';
+import 'package:zefyrka/zefyrka.dart';
 
 import '../testing.dart';
+
+Future<void> _updateEditingState(
+    WidgetTester tester, TextEditingValue value) async {
+  // Client id -1 bypasses the client id verification in debug builds.
+  await tester.binding.defaultBinaryMessenger.handlePlatformMessage(
+    SystemChannels.textInput.name,
+    SystemChannels.textInput.codec.encodeMethodCall(MethodCall(
+      'TextInputClient.updateEditingState',
+      <dynamic>[-1, value.toJSON()],
+    )),
+    (ByteData? _) {},
+  );
+  await tester.pump();
+}
 
 void main() {
   testWidgets('keyboard replacing all text including last line break',
@@ -12,25 +28,37 @@ void main() {
     // The platform text includes the last line break of the document.
     final text = editor.controller.plainTextEditingValue.text;
 
-    // Client id -1 bypasses the client id verification in debug builds.
-    await tester.binding.defaultBinaryMessenger.handlePlatformMessage(
-      SystemChannels.textInput.name,
-      SystemChannels.textInput.codec.encodeMethodCall(MethodCall(
-        'TextInputClient.updateEditingState',
-        <dynamic>[
-          -1,
-          const TextEditingValue(
-            text: 'x',
-            selection: TextSelection.collapsed(offset: 1),
-          ).toJSON(),
-        ],
-      )),
-      (ByteData? _) {},
+    await _updateEditingState(
+      tester,
+      const TextEditingValue(
+        text: 'x',
+        selection: TextSelection.collapsed(offset: 1),
+      ),
     );
-    await tester.pump();
 
     expect(tester.takeException(), isNull);
     expect(text, isNot('x'));
     expect(editor.document.toDelta(), Delta()..insert('x\n'));
+  });
+
+  testWidgets('keyboard deleting all text keeps the editor usable',
+      (tester) async {
+    final editor = EditorSandBox(tester: tester);
+    await editor.pumpAndTap();
+
+    // Select all and delete on the keyboard removes the last line break too.
+    await _updateEditingState(
+      tester,
+      const TextEditingValue(
+        text: '',
+        selection: TextSelection.collapsed(offset: 0),
+      ),
+    );
+    expect(tester.takeException(), isNull);
+    expect(editor.document.toDelta(), Delta()..insert('\n'));
+
+    await tester.tapAt(tester.getCenter(find.byType(ZefyrField)));
+    await tester.pump();
+    expect(tester.takeException(), isNull);
   });
 }
